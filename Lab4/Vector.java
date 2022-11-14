402d3d3697bd04caa7f4e0f66be0334685f40f91
Mia0311 <phuongminh.nguyen01@estudiant.upf.edu>
2022-11-14 19:03:27 +0100

    vector

--- Lab4/Vector.java
+++ Lab4/Vector.java
@@ -1,3 +1,39 @@
+import java.util.*;
+
 public class Vector {
-    
+    private double[] values;
+    private int dim;
+
+    public Vector(int d) {
+        this.dim = d;
+        this.values = new double[dim]; 
+    }
+
+    public void set(int i, double val) {
+        values[i] = val;
+    }
+
+    public double get(int i) {
+        return values[i];
+    }
+
+    public void zero() {
+        for (int i=0; i<dim; i++){
+            values[i] = 0;
+        }
+        
+    }
+
+    public void multiply(double s) {
+        for (int i=0; i<dim; i++){
+            values[i] = values[i] * s; 
+        }
+    }
+
+    public void print() {
+        for (int i=0; i<dim; i++){
+            System.out.print(values[i]);
+        }
+    }
+
 }
