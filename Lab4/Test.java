9cecc23c0b4472af7fe3111733bbc2de200f11e0
wangyuyan03 <yuyan.wang01@estudiant.upf.edu>
2022-11-24 23:27:27 +0100

    improve ui

--- Lab4/Test.java
+++ Lab4/Test.java
@@ -39,35 +39,32 @@ public class Test extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e){
         if (this.imagePanel==null){
-            System.out.println("You actually do not have any image! The program can not do any action");
+            printDialogBox("You currently do not have any image! The program can not do any action.", 500, 120);
             return;
         }
-        if (e.getSource()==increaseB){
+        if (e.getSource() == increaseB){
             this.changeBrightness(1.05);
-            JDialog d = new JDialog(this, "message from the program");
-            // create a label
-            JLabel l = new JLabel("You have succesfully incremented the brightness of the image!");
-            l.setHorizontalAlignment(JLabel.CENTER);
-            d.add(l);
-            // setsize of dialog
-            d.setSize(450, 120);
-            d.setLocation(500, 100);
-            // set visibility of dialog
-            d.setVisible(true);
+            printDialogBox("You have succesfully incremented the brightness of the image!",500, 120);
 
         } else if (e.getSource()==decreaseB){
             this.changeBrightness(0.95);
-            JDialog d = new JDialog(this, "message from the program");
-            // create a label
-            JLabel l = new JLabel("You have successfully decreased the brightness of the image!");
-            l.setHorizontalAlignment(JLabel.CENTER);
-            d.add(l);
-            // setsize of dialog
-            d.setSize(450, 120);
-            d.setLocation(500, 100);
-            // set visibility of dialog
-            d.setVisible(true);
+            printDialogBox("You have succesfully decreased the brightness of the image!", 500, 120);
         }
+    }
+
+    private void printDialogBox(String message, int width, int height){
+        JDialog d = new JDialog(this, "message from the program");
+        // Create a label
+        String temp = "<html>" + message + "</html>";
+        JLabel l = new JLabel(temp);
+        l.setHorizontalAlignment(JLabel.CENTER);
+        d.add(l);
+        // Set size of dialog box
+        d.setSize(width, height);
+        // Set location
+        d.setLocation(500, 100);
+        // Set visibility of dialog
+        d.setVisible(true);
     }
 
     public void display(){
@@ -78,6 +75,7 @@ public class Test extends JFrame implements ActionListener{
     }
 
     public void addImageToWindow(String path, Boolean colored){
+        printDialogBox("The program is loading the image, the process may take few seconds...", 500, 120);
         // Each Window is only capable of showing one image
         // Delete (if there exists) the previous image
         deleteImage();
@@ -87,6 +85,8 @@ public class Test extends JFrame implements ActionListener{
         this.getContentPane().add(imagePanel);
         // Display the changes
         this.display();
+        printDialogBox("The image has been successfully loaded. It you don't see the image on the windows, please do not hesitate. " +
+                        "If you click on the buttoms for many times, you will see the image displayed. " + "If you don't see any button, rerun the whole program", 600, 500);
     }
 
     // Overloading the previous method to be able to create an ImagePanel from a given Frame (matrix)
