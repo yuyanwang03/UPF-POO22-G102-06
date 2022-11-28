022037ee1841a190c5a0a4c436d6b261794fe8ab
Mia0311 <phuongminh.nguyen01@estudiant.upf.edu>
2022-11-28 17:56:35 +0100

    entity

---
+++ Lab5/Entity.cpp
@@ -0,0 +1,39 @@
+#ifndef _ENTITY_
+#define _ENTITY
+
+// i was having error for the include parts tho
+
+#include <string>
+#include <cmath>
+
+class Entity {
+    private:
+        Vec2D pos;
+        string name;
+        int energy;
+
+    public:
+        void Entity(Vec2D p, string n, int e) {
+            pos = p;
+            name = n;
+            energy = e;
+        }
+
+        string getName() {
+            return n;
+        }
+
+        void setName(string n) {
+            name = n;
+        }
+
+        int compareTo(Entity e){
+            //?
+        }
+
+        virtual void update() = 0;
+};
+
+#endif 
+
+///
